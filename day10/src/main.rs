0634cf04d54b0f0d474601aeec42b4cf8e8fef3d
Roosevelt Burden <rooseveltburden@gmail.com>
2023-12-10 15:52:13 -0700

    day10 still rough

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -18,128 +18,144 @@ fn part_1(input: &String) -> usize {
 }
 
 fn bfs(matrix: &Vec<Vec<Pipe>>) -> usize {
+    let start = find_start(matrix);
+
+    let mut visited = HashSet::from([(start.x, start.y)]);
+    let mut queue = VecDeque::from([(start.clone(), 0)]);
+
     let mut max_len = 0;
 
-    let (x, y) = find_start(matrix);
-    let start = matrix[x][y];
+    while let Some((pipe, len)) = queue.pop_front() {
+        max_len = len;
 
-    let mut queue = VecDeque::from([(start, 0, HashSet::from([start]))]);
-
-    while let Some((pipe, len, mut visited)) = queue.pop_front() {
-        println!("[pipe]: {pipe:?} {len}");
-        max_len = max_len.max(len);
-
-        if pipe == start && len > 0 {
-            println!("[end]: {pipe:?}");
-            return len;
-        }
-
-        let iter: Vec<_> = pipe
-            .adjacent_cells()
+        pipe.find_adjacent(matrix)
             .into_iter()
-            .filter(|&(r, c)| r < matrix.len() && c < matrix[0].len())
-            .filter(|&(r, c)| matrix[r][c].is_adjacent(&pipe))
-            .map(|(r, c)| matrix[r][c])
-            .filter(|&pos| visited.insert(pos))
-            .map(|p| (p, len + 1))
-            .collect();
-
-        queue.extend(iter.into_iter().map(|(p, l)| (p, l, visited.clone())));
+            // .inspect(|p| println!("[pipe]: {p:?}"))
+            .filter(|p| visited.insert((p.x, p.y)))
+            .for_each(|p| queue.push_back((p, len + 1)));
     }
 
     max_len
 }
 
-fn find_start(matrix: &Vec<Vec<Pipe>>) -> Pos {
+fn find_start(matrix: &Vec<Vec<Pipe>>) -> Pipe {
     matrix
         .into_iter()
-        .find_map(|r| {
-            r.into_iter().find_map(|p| match p {
-                Pipe::Start(pos) => true.then(|| *pos),
-                _ => None,
-            })
-        })
+        .find_map(|row| row.into_iter().find(|p| p.shape == PipeShape::Start))
+        .map(|p| p.clone())
         .unwrap()
 }
 
 fn parse_input(input: &String) -> Vec<Vec<Pipe>> {
-    input.lines().enumerate().map(parse_line).collect()
+    let lines: Vec<_> = input.lines().collect();
+    let grid_size = (lines.len() as i32, lines[0].len() as i32);
+    let parse_line = make_parse_line(grid_size);
+
+    lines.into_iter().enumerate().map(parse_line).collect()
 }
 
-fn parse_line((row, line): (usize, &str)) -> Vec<Pipe> {
-    line.char_indices()
-        .map(|(col, s)| ((row, col), s.to_string()))
-        .map(Pipe::from_str)
-        .collect()
+fn make_parse_line(grid_size: (i32, i32)) -> impl Fn((usize, &str)) -> Vec<Pipe> {
+    move |(row, line): (usize, &str)| {
+        line.char_indices()
+            .map(|(col, c)| Pipe::from_char(c, (row, col), grid_size))
+            .collect()
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-enum Pipe {
-    Start(Pos),
-    Ground(Pos),
-    Vertical(Pos),
-    Horizontal(Pos),
-    NorthWest(Pos),
-    NorthEast(Pos),
-    SouthWest(Pos),
-    SouthEast(Pos),
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+struct Pipe {
+    x: usize,
+    y: usize,
+    m: i32,
+    n: i32,
+    shape: PipeShape,
 }
 
 impl Pipe {
-    fn from_str((pos, s): (Pos, String)) -> Self {
-        match s.as_str() {
-            "S" => Self::Start(pos),
-            "." => Self::Ground(pos),
-            "|" => Self::Vertical(pos),
-            "-" => Self::Horizontal(pos),
-            "J" => Self::NorthWest(pos),
-            "L" => Self::NorthEast(pos),
-            "7" => Self::SouthWest(pos),
-            "F" => Self::SouthEast(pos),
-            _ => panic!("error parsing pipe"),
+    fn from_char(c: char, (x, y): (usize, usize), (m, n): (i32, i32)) -> Self {
+        Self {
+            x,
+            y,
+            m,
+            n,
+            shape: PipeShape::from_char(c),
         }
     }
 
-    fn is_adjacent(&self, other: &Self) -> bool {
-        other.adjacent_cells().contains(&self.get_pos())
-    }
-
-    fn adjacent_cells(&self) -> HashSet<Pos> {
+    fn find_adjacent(&self, matrix: &Vec<Vec<Pipe>>) -> HashSet<Pipe> {
+        let valid_shapes = self.shape.connects_to();
         let (north, south, east, west) = ((-1, 0), (1, 0), (0, 1), (0, -1));
 
-        let coord = |&(x, y): &Pos, dirs: &[(i32, i32)]| {
+        let get_adj = |dirs: &[(i32, i32)]| {
+            let (x, y) = (self.x as i32, self.y as i32);
             dirs.into_iter()
-                .filter_map(|&(a, b)| {
-                    let (x, y) = (a + x as i32, b + y as i32);
-                    (x >= 0 && y >= 0).then(|| (x as usize, y as usize))
-                })
+                .map(|(a, b)| (a + x, b + y))
+                .filter(|&(a, b)| a >= 0 && a < self.m && b >= 0 && b < self.n)
+                .map(|(r, c)| (r as usize, c as usize))
+                .map(|(r, c)| matrix[r][c].clone())
+                // .inspect(|p| println!("{p:?}"))
+                .filter(|p| valid_shapes.contains(&p.shape))
                 .collect()
         };
 
-        match self {
-            Self::Start(pos) => coord(pos, &[north, south, east, west]),
-            Self::Ground(pos) => coord(pos, &[]),
-            Self::Vertical(pos) => coord(pos, &[north, south]),
-            Self::Horizontal(pos) => coord(pos, &[east, west]),
-            Self::NorthWest(pos) => coord(pos, &[north, west]),
-            Self::NorthEast(pos) => coord(pos, &[north, east]),
-            Self::SouthWest(pos) => coord(pos, &[south, west]),
-            Self::SouthEast(pos) => coord(pos, &[south, east]),
-        }
-    }
-
-    fn get_pos(&self) -> Pos {
-        *match self {
-            Self::Start(pos) => pos,
-            Self::Ground(pos) => pos,
-            Self::Vertical(pos) => pos,
-            Self::Horizontal(pos) => pos,
-            Self::NorthWest(pos) => pos,
-            Self::NorthEast(pos) => pos,
-            Self::SouthWest(pos) => pos,
-            Self::SouthEast(pos) => pos,
+        match self.shape {
+            PipeShape::Start => get_adj(&[north, south, east, west]),
+            PipeShape::Ground => get_adj(&[]),
+            PipeShape::Horizontal => get_adj(&[east, west]),
+            PipeShape::Vertical => get_adj(&[north, south]),
+            PipeShape::NorthWest => get_adj(&[north, west]),
+            PipeShape::NorthEast => get_adj(&[north, east]),
+            PipeShape::SouthWest => get_adj(&[south, west]),
+            PipeShape::SouthEast => get_adj(&[south, east]),
         }
     }
 }
 
-type Pos = (usize, usize);
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+enum PipeShape {
+    Start,
+    Ground,
+    Horizontal,
+    Vertical,
+    NorthWest,
+    NorthEast,
+    SouthWest,
+    SouthEast,
+}
+
+impl PipeShape {
+    fn from_char(c: char) -> Self {
+        use PipeShape::*;
+
+        match c {
+            'S' => Start,
+            '.' => Ground,
+            '-' => Horizontal,
+            '|' => Vertical,
+            'J' => NorthWest,
+            'L' => NorthEast,
+            '7' => SouthWest,
+            'F' => SouthEast,
+            _ => panic!("error parsing PipeShape"),
+        }
+    }
+
+    fn connects_to(&self) -> HashSet<PipeShape> {
+        use PipeShape::*;
+
+        match self {
+            Start => vec![
+                Vertical, Horizontal, NorthEast, NorthWest, SouthEast, SouthWest,
+            ],
+            Ground => vec![],
+            Horizontal => vec![Horizontal, NorthEast, NorthWest, SouthEast, SouthWest],
+            Vertical => vec![Vertical, NorthEast, NorthWest, SouthEast, SouthWest],
+            NorthWest => vec![Vertical, Horizontal, NorthEast, SouthEast, SouthWest],
+            NorthEast => vec![Vertical, Horizontal, NorthWest, SouthEast, SouthWest],
+            SouthWest => vec![Vertical, Horizontal, NorthWest, SouthEast, NorthEast],
+            SouthEast => vec![Vertical, Horizontal, NorthWest, SouthWest, NorthEast],
+        }
+        .into_iter()
+        .collect()
+    }
+}
